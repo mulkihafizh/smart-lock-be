b943b9d7cb7dd0aee720065e45317145a6223b11
MulkiHafizh <ukiyo142@gmail.com>
2023-09-25 16:13:42 +0700

    vercel

--- routes/door.js
+++ routes/door.js
@@ -4,11 +4,13 @@ const {
   getDoor,
   getOne,
   unlockDoor,
+  lockDoor,
 } = require("../controllers/doorController");
 const { check } = require("express-validator");
 const router = express.Router();
 
 router.post("/door/create", createDoor);
+router.post("/door/lock", lockDoor);
 router.post("/door/unlock", unlockDoor);
 router.get("/door/get", getDoor);
 router.get("/door/get/:id", getOne);
